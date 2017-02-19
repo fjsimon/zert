4e19eadd6acca37881a309790fb0fd01f12c645e
fjsimon <fjlopez.mail@gmail.com>
2017-02-19 18:33:53 +0000

    Added java.time examples

---
+++ src/main/java/com/oca/exam/core/DateTimeFormattingExample.java
@@ -0,0 +1,33 @@
+package com.oca.exam.core;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class DateTimeFormattingExample {
+
+    public static void main(String... args) {
+
+        LocalDate nowLocalDate = LocalDate.now();
+        System.out.printf("Now date in ISO date format: %s %n",
+                nowLocalDate.format(DateTimeFormatter.ISO_DATE));
+        System.out.printf("Now date/time in ISO date/time format: %s %n",
+                LocalDateTime.now().format(DateTimeFormatter.ISO_DATE_TIME));
+
+        String ddMMyyyyPattern = "dd/MM/yyyy";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(ddMMyyyyPattern);
+        String formattedDate = nowLocalDate.format(formatter);
+        System.out.printf("Now date in [%s] format: %s %n", ddMMyyyyPattern, formattedDate);
+        System.out.printf("Parsed formatted now date[%s]: %s %n", formattedDate,
+                LocalDate.parse(formattedDate, formatter));
+
+        // http://www.localeplanet.com/java/fr-CA/
+        String dMMMMyyyyPattern = "d MMMM yyyy";
+        DateTimeFormatter canadianFrenchFormatter =
+                DateTimeFormatter.ofPattern(dMMMMyyyyPattern, Locale.CANADA_FRENCH);
+        System.out.printf("Now date in [%s] format and [%s] locale: %s %n",
+                dMMMMyyyyPattern, Locale.CANADA_FRENCH,
+                nowLocalDate.format(canadianFrenchFormatter));
+    }
+}
