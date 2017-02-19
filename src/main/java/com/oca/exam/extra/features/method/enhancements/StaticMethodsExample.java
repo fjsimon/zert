c085df78f8a5f1fb577c5e7428c88ea302460af7
fjsimon <fjlopez.mail@gmail.com>
2017-02-19 18:43:44 +0000

    Added method enhancements examples

---
+++ src/main/java/com/oca/exam/extra/features/method/enhancements/StaticMethodsExample.java
@@ -0,0 +1,43 @@
+package com.oca.exam.extra.features.method.enhancements;
+
+import static java.lang.String.format;
+
+public class StaticMethodsExample {
+
+    public static void main(String... args) {
+
+        Greeting customGreeting = new CustomGreeting();
+        customGreeting.setName("Fran");
+        System.out.println(customGreeting.getGreeting());
+    }
+}
+
+interface Greeting {
+
+    void setName(String name);
+
+    String getGreeting();
+
+    // static method in question
+    static String getTemplate() {
+
+        return "Hello, %s!";
+    }
+}
+
+class CustomGreeting implements Greeting {
+
+    private String name;
+
+    @Override
+    public void setName(String name) {
+
+        this.name = name;
+    }
+
+    @Override
+    public String getGreeting() {
+
+        return format(Greeting.getTemplate(), name);
+    }
+}
