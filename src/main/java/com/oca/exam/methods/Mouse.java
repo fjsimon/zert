0162659cceec55d1c6973e38919449320e7aa35f
fjsimon <fjlopez.mail@gmail.com>
2017-08-30 20:46:54 +0100

    Added methods examples

---
+++ src/main/java/com/oca/exam/methods/Mouse.java
@@ -0,0 +1,31 @@
+package com.oca.exam.methods;
+
+public class Mouse {
+
+    private int numTeeth;
+    private int numWhiskers;
+    private int weight;
+
+    public Mouse(int weight) {
+        this(weight, 16);
+    }
+
+    public Mouse(int weight, int numTeeth) {
+        this(weight, numTeeth, 6);
+    }
+
+    public Mouse(int weight, int numTeeth, int numWhiskers) {
+        this.weight = weight;
+        this.numTeeth = numTeeth;
+        this.numWhiskers = numWhiskers;
+    }
+
+    public void print() {
+        System.out.printf("%s %s %s %n", weight, numTeeth, numWhiskers);
+    }
+
+    public static void main(String...args){
+        Mouse m = new Mouse(15);
+        m.print();
+    }
+}
