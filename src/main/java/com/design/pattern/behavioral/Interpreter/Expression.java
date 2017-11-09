8b6f07c2ede3c6593a5380576b8c9aa4fa7fa19f
fjsimon <fjlopez.mail@gmail.com>
2017-11-09 20:37:21 +0000

    Added Interpreter (design pattern behavioral)

---
+++ src/main/java/com/design/pattern/behavioral/Interpreter/Expression.java
@@ -0,0 +1,6 @@
+package com.design.pattern.behavioral.Interpreter;
+
+public interface Expression {
+
+    String interpret(InterpreterContext ic);
+}
---
+++ src/main/java/com/design/pattern/behavioral/Interpreter/InterpreterContext.java
@@ -0,0 +1,14 @@
+package com.design.pattern.behavioral.Interpreter;
+
+public class InterpreterContext {
+
+    public String getBinaryFormat(int i) {
+
+        return Integer.toBinaryString(i);
+    }
+
+    public String getHexadecimalFormat(int i) {
+
+        return Integer.toHexString(i);
+    }
+}
