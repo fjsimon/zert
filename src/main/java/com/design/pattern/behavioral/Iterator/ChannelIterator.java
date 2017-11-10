92f578d1487a804bd7051d900991a44041ab3798
fjsimon <fjlopez.mail@gmail.com>
2017-11-10 21:49:05 +0000

    Added Iterator (design pattern behavioral)

---
+++ src/main/java/com/design/pattern/behavioral/Iterator/ChannelIterator.java
@@ -0,0 +1,8 @@
+package com.design.pattern.behavioral.Iterator;
+
+public interface ChannelIterator {
+
+    public boolean hasNext();
+
+    public Channel next();
+}
---
+++ src/main/java/com/design/pattern/behavioral/Iterator/ChannelTypeEnum.java
@@ -0,0 +1,6 @@
+package com.design.pattern.behavioral.Iterator;
+
+public enum ChannelTypeEnum {
+
+    ENGLISH, HINDI, FRENCH, ALL;
+}
