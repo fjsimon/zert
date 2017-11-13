e71304562e3df857d6915c5f6c24a34c84ef9c39
fjsimon <fjlopez.mail@gmail.com>
2017-11-13 19:57:44 +0000

    Added new tests to Test07

--- src/test/java/com/oca/exam/Test07.java
+++ src/test/java/com/oca/exam/Test07.java
@@ -5,6 +5,9 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TestName;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.time.*;
 import java.util.ArrayList;
 import java.util.function.Predicate;
 
@@ -19,8 +22,8 @@ public class Test07 {
         System.out.printf("Running %s:%n", name.getMethodName());
     }
 
-    public void processList(ArrayList<DataHolder> dataList, Process p){
-        for(DataHolder d: dataList){
+    public void processList(ArrayList<DataHolder> dataList, Process p) {
+        for (DataHolder d : dataList) {
             p.process(d.value, d.value);
         }
     }
@@ -33,27 +36,33 @@ public class Test07 {
         al.add(new DataHolder(2));
         al.add(new DataHolder(3));
 
-        processList(al, (int a, int b)->System.out.println(a*b) );
+        processList(al, (int a, int b) -> System.out.println(a * b));
 
-        processList(al, (a, b)->System.out.println(a*b));
+        processList(al, (a, b) -> System.out.println(a * b));
 
-        processList(al, (a, b) ->{  System.out.println(a*b); } );
+        processList(al, (a, b) -> {
+            System.out.println(a * b);
+        });
     }
 
     @Test
     public void q05() {
 
         boolean[] b1 = new boolean[2];
-        boolean[] b2 = {true , false};
-        System.out.println( "" + (b1[0] == b2[0]) + ", "+ (b1[1] == b2[1])  );
+        boolean[] b2 = {true, false};
+        System.out.println("" + (b1[0] == b2[0]) + ", " + (b1[1] == b2[1]));
     }
 
     @Test
     public void q06() {
 
-        for( ;Math.random()<0.5; ){ System.out.println("true"); }
+        for (; Math.random() < 0.5; ) {
+            System.out.println("true");
+        }
 
-        for(;;){ if(Math.random()<.05) break; }
+        for (; ; ) {
+            if (Math.random() < .05) break;
+        }
 
 //        for(;;Math.random()){ System.out.println("true"); }
     }
@@ -83,10 +92,16 @@ public class Test07 {
 //        int condition = 2;
 //        Byte condition = 1;
         Integer condition = new Integer("1");
-        switch( condition ){
-            case 1  : System.out.println("1");   break;
-            case 2  : System.out.println("2");   break;
-            case 3 : System.out.println("3");  break;
+        switch (condition) {
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
         }
 
     }
@@ -94,7 +109,7 @@ public class Test07 {
     @Test
     public void q17() {
 
-        String[][][] arr  ={{ { "a", "b" , "c"}, { "d", "e", null } },{ {"x"}, null },{{"y"}},{ { "z","p"}, {} }};
+        String[][][] arr = {{{"a", "b", "c"}, {"d", "e", null}}, {{"x"}, null}, {{"y"}}, {{"z", "p"}, {}}};
         System.out.println(arr[0][1][2]);
     }
 
@@ -104,9 +119,9 @@ public class Test07 {
         int value = Integer.parseInt(null);
     }
 
-    public void printUsefulData(ArrayList<Data> dataList, Predicate<Data> p){
-        for(Data d: dataList){
-            if(p.test(d)) System.out.println(d.value);
+    public void printUsefulData(ArrayList<Data> dataList, Predicate<Data> p) {
+        for (Data d : dataList) {
+            if (p.test(d)) System.out.println(d.value);
         }
     }
 
@@ -118,9 +133,11 @@ public class Test07 {
         al.add(new Data(2));
         al.add(new Data(3));
 
-        printUsefulData(al, d-> d.value>2 );
+        printUsefulData(al, d -> d.value > 2);
 
-        printUsefulData(al, (Data d)-> { return d.value>2; }  );
+        printUsefulData(al, (Data d) -> {
+            return d.value > 2;
+        });
 
     }
 
@@ -130,7 +147,7 @@ public class Test07 {
         try {
             RuntimeException re = null;
             throw re;
-        } catch(Exception e) {
+        } catch (Exception e) {
             System.out.println(e);
         }
     }
@@ -148,18 +165,84 @@ public class Test07 {
     public void q26() {
 
         Object t = new Integer(106);
-        int k = ((Integer) t).intValue()/10;
+        int k = ((Integer) t).intValue() / 10;
         System.out.println(k); // int
 
-        System.out.println(100/9.9); // double
+        System.out.println(100 / 9.9); // double
 
-        System.out.println(100/10.0); // double
+        System.out.println(100 / 10.0); // double
 
-        System.out.println(100/10); // int
+        System.out.println(100 / 10); // int
 
         System.out.println(3 + 100 / 10 * 2 - 13); // int => 3 + ((100 / 10) * 2) - 13
     }
 
+    public static void ifTest(boolean flag) {
+        if (flag)   //1
+            if (flag)   //2
+                if (flag)   //3
+                    System.out.println("False True");
+                else        //4
+                    System.out.println("True False");
+            else        //5
+                System.out.println("True True");
+        else        //6
+            System.out.println("False False");
+    }
+
+    @Test
+    public void q31() {
+
+        ifTest(true);
+        ifTest(false);
+    }
+
+    @Test
+    public void q32() {
+
+        LocalDateTime ld = LocalDateTime.of(2015, Month.OCTOBER, 31, 10, 0);
+
+        ZonedDateTime date = ZonedDateTime.of(ld, ZoneId.of("US/Eastern"));
+        date = date.plus(Duration.ofDays(1));
+        System.out.println(date);
+
+        date = ZonedDateTime.of(ld, ZoneId.of("US/Eastern"));
+        date = date.plus(Period.ofDays(1));
+        System.out.println(date);
+    }
+
+    @Test
+    public void q36() {
+
+        StringBuilder sb = new StringBuilder("asdf");
+        String str1 = sb.toString();
+        String str2 = sb.toString();
+        System.out.println(str1 == str2);
+
+        str1 = sb.toString();
+        str2 = str1;
+        System.out.println(str1 == str2);
+
+//        String str1 = sb.toString();
+//        System.out.println(str1 == sb);
+
+        System.out.println(sb == new StringBuilder(sb));
+    }
+
+    @Test
+    public void q37() {
+
+        int i = 0, j = 11;
+        do {
+            System.out.println(i + "  " + j);
+            if (i > j) {
+                break;
+            }
+            j--;
+        } while (++i < 5);
+
+        System.out.println(i + "  " + j);
+    }
 }
 
 
@@ -169,20 +252,29 @@ interface Process {
 
 class DataHolder {
     int value;
-    DataHolder(int value){
+
+    DataHolder(int value) {
         this.value = value;
     }
 }
 
 class T06Q02 {
 
-    void probe(int... x) { System.out.println("In ..."); }  //1
+    void probe(int... x) {
+        System.out.println("In ...");
+    }  //1
 
-    void probe(Integer x) { System.out.println("In Integer"); } //2
+    void probe(Integer x) {
+        System.out.println("In Integer");
+    } //2
 
-    void probe(long x) { System.out.println("In long"); } //3
+    void probe(long x) {
+        System.out.println("In long");
+    } //3
 
-    void probe(Long x) { System.out.println("In LONG"); } //4
+    void probe(Long x) {
+        System.out.println("In LONG");
+    } //4
 
     public static void main(String[] args) {
 
@@ -205,33 +297,37 @@ class T06Q04 {
         return null;
     }
 
-    public static void main(String[ ] args) {
-        System.out.println( getDatabase().url );
+    public static void main(String[] args) {
+        System.out.println(getDatabase().url);
     }
 }
 
-class BaseClassQ09{
+class BaseClassQ09 {
 
     public void print(String s) {
-        System.out.println("BaseClass : "+s);
+        System.out.println("BaseClass : " + s);
     }
 }
 
-class SubClassQ09 extends BaseClassQ09{
+class SubClassQ09 extends BaseClassQ09 {
 
-    public void print(String s){
-        System.out.println("SubClass : "+s);
+    public void print(String s) {
+        System.out.println("SubClass : " + s);
         super.print(s);
     }
 
-    public static void main(String args[]){
+    public static void main(String args[]) {
         SubClassQ09 sc = new SubClassQ09();
         sc.print("location");
     }
 }
 
-class NewException extends Exception {}
-class AnotherException extends Exception {}
+class NewException extends Exception {
+}
+
+class AnotherException extends Exception {
+}
+
 class T07Q23 {
 
     public static void main(String[] args) throws Exception {
@@ -272,5 +368,40 @@ class T07Q28 {
     static void m1() {
         System.out.println("m1 Starts");
         throw new IndexOutOfBoundsException("Big Bang ");
+    }
+}
+
+class T07Q33 {
+    public static void main(String[] args) {
+        calculate(2);
+    }
+
+    public static void calculate(int x) {
+        String val;
+        switch (x) {
+            case 2:
+            default:
+                val = "def";
+        }
+        System.out.println(val);
+    }
+}
+
+ class T07Q41 {
+
+    public void testRefs(String str, StringBuilder sb) {
+
+        str = str + sb.toString();
+        sb.append(str);
+        str = null;
+        sb = null;
+    }
+
+    public static void main(String[] args) {
+
+        String s = "aaa";
+        StringBuilder sb = new StringBuilder("bbb");
+        new T07Q41().testRefs(s, sb);
+        System.out.println("s="+s+" sb="+sb);
     }
 }
