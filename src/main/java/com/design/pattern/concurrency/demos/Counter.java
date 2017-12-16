076177920e89c4a4228ac688d643cd71dda6ce44
fjsimon <fjlopez.mail@gmail.com>
2017-12-16 20:15:00 +0000

    Added concurrency examples.

---
+++ src/main/java/com/design/pattern/concurrency/demos/Counter.java
@@ -0,0 +1,20 @@
+package com.design.pattern.concurrency.demos;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Counter {
+
+    private AtomicInteger value = new AtomicInteger();
+
+    public int getValue() {
+        return value.get();
+    }
+
+    public int increment() {
+        int readValue = value.get();
+        while (!value.compareAndSet(readValue, readValue+1)) {
+            readValue = value.get();
+        }
+        return readValue+1;
+    }
+}
