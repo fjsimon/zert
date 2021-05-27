547dd19f0e53576b1e6951317538d3046d0c8e27
fjsimon <fjlopez.mail@gmail.com>
2021-05-27 09:44:20 +0100

    Refactor: moving src to test (local date examples)

--- src/test/java/com/oca/exam/core/LocalDateTest.java
+++ src/test/java/com/oca/exam/core/LocalDateTest.java
@@ -3,8 +3,17 @@ package com.oca.exam.core;
 import org.junit.Test;
 
 import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.time.temporal.ChronoField;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalUnit;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Optional;
+
+import static java.time.temporal.ChronoUnit.*;
 
 public class LocalDateTest {
 
@@ -25,6 +34,138 @@ public class LocalDateTest {
                 todayDate.get(ChronoField.YEAR),
                 todayDate.get(ChronoField.MONTH_OF_YEAR),
                 todayDate.get(ChronoField.DAY_OF_MONTH));
+
+        System.out.printf("Local Date at : %s %n", LocalDate.now());
+        System.out.printf("Local Date at : %s %n", LocalDate.of(2015, Month.JANUARY, 20));
+        System.out.printf("Local Date at : %s %n", LocalDate.of(2015, 1, 20));
+        System.out.printf("Local Date now plus 2 days : %s %n", LocalDate.now().plusDays(2));
+        System.out.printf("Local Date now plus 2 weeks : %s %n", LocalDate.now().plusWeeks(2));
+        System.out.printf("Local Date now plus 2 months : %s %n", LocalDate.now().plusMonths(2));
+        System.out.printf("Local Date now plus 2 years : %s %n", LocalDate.now().plusYears(2));
+
+//        LocalDate d = new LocalDate(); // DOES NOT COMPILE
+//        LocalDate.of(2015, Month.JANUARY, 32); // java.time.DateTimeException
+
+    }
+
+    @Test
+    public void getLocalDateFormatTest() {
+
+        LocalDate today = LocalDate.now();
+        int year = today.getYear();
+        int month = today.getMonthValue();
+        int day = today.getDayOfMonth();
+        System.out.println(String.format("Year : %d Month : %d day : %d", year, month, day));
+    }
+
+    @Test
+    public void getMonthDayTest() {
+
+        LocalDate today = LocalDate.now();
+        LocalDate dateOfBirth = LocalDate.of(today.getYear(), today.getMonthValue(), today.getDayOfMonth());
+        System.out.println(MonthDay.of(dateOfBirth.getMonth(), dateOfBirth.getDayOfMonth()));
+    }
+
+
+    @Test
+    public void getLocalDatePlusTest() {
+
+        LocalDate today = LocalDate.now();
+        System.out.println("Period (days) " +  Period.between(today, today.plus(1L, WEEKS)).getDays());
+        System.out.println("Period (days) " +  Period.between(today, today.plus(1L, DAYS)).getDays());
+        System.out.println("Period (years) " +  Period.between(today, today.plus(1L, YEARS)).getYears());
+    }
+
+    @Test
+    public void getLocalDateMinusTest() {
+
+        LocalDate today = LocalDate.now();
+        System.out.println("Period (days) " +  Period.between(today, today.minus(1L, WEEKS)).getDays());
+        System.out.println("Period (days) " +  Period.between(today, today.minus(1L, DAYS)).getDays());
+        System.out.println("Period (years) " +  Period.between(today, today.minus(1L, YEARS)).getYears());
+    }
+
+    @Test
+    public void isLeapYearTest() {
+
+        System.out.println("isLeapYear " + LocalDate.now().isLeapYear());
+    }
+
+    @Test
+    public void getDifferenceTest() {
+
+        LocalDate java8Release = LocalDate.of(2014, Month.MARCH, 14);
+        LocalDate today = LocalDate.now();
+        Period periodToNextJavaRelease = Period.between(java8Release, today);
+        System.out.println(periodToNextJavaRelease);
+    }
+
+    @Test
+    public void formatDateTest() {
+
+        String date = "20140116";
+        LocalDate localDateFormatted = LocalDate.parse(date, DateTimeFormatter.BASIC_ISO_DATE);
+        System.out.printf("Date generated from String %s is %s %n", date, localDateFormatted);
+    }
+
+    // Old way
+    public static Date addDay(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        cal.add(Calendar.DATE, 1);
+        return cal.getTime();
+    }
+
+    // New way
+    public static LocalDate addDay(LocalDate date){
+        return date.plusDays(1);
+    }
+
+    @Test
+    public void addDaysTest() {
+
+        System.out.printf("Date now plus 1 day : %s %n", addDay(new Date()));
+        System.out.printf("LocalDate now plus 1 day: %s %n", addDay(LocalDate.now()));
+    }
+
+    private static Optional<LocalDate> parseDate(String date) {
+
+        try {
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy");
+            LocalDate parseDate = LocalDate.parse(date, formatter);
+            return Optional.of(parseDate);
+        } catch (DateTimeParseException ex) {
+            ex.printStackTrace();
+        }
+
+        return Optional.empty();
+    }
+
+    @Test
+    public void parseDateTest() {
+
+        String parsedate = "Apr 18 2014";
+        LocalDate localDate = parseDate(parsedate).get();
+        System.out.printf("Successfully parsed String (%s), date is (%s)%n", parsedate, localDate);
+    }
+
+    private static Optional<String> getLocalDateTimeToString(LocalDateTime localDateTime) {
+
+        try {
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("MMM dd yyyy hh:mm a");
+            String landing = localDateTime.format(format);
+            return Optional.of(landing);
+        } catch (DateTimeException ex) {
+            ex.printStackTrace();
+        }
+        return Optional.empty();
+    }
+
+    @Test
+    public void localDateTimeToStringTest() {
+
+        String localDateString = getLocalDateTimeToString(LocalDateTime.now()).get();
+        System.out.printf("Local Date at : %s %n", localDateString);
     }
 
     @Test
@@ -92,7 +233,7 @@ public class LocalDateTest {
     }
 
     @Test
-    public void toEpochtest() {
+    public void toEpochTest() {
 
         System.out.printf("Number of days since January 1, 1970: %s %n", LocalDate.now().toEpochDay());
         System.out.printf("Number of seconds since January 1, 1970: %s %n",
