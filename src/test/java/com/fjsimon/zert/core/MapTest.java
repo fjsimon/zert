137f3f262bbe2c305777b62ba4e5af848f5a7606
fjsimon <fjlopez.mail@gmail.com>
2021-05-27 15:00:43 +0100

    Refactor: moving src to test (map examples)

--- src/test/java/com/fjsimon/zert/core/MapTest.java
+++ src/test/java/com/fjsimon/zert/core/MapTest.java
@@ -1,38 +1,38 @@
 package com.fjsimon.zert.core;
 
+import org.junit.Test;
 
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class MapExamples {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.Is.is;
+
+public class MapTest {
 
     static class Stack {
+
         String name;
         Stack(String name){
             this.name = name;
         }
 
         public boolean equals(Object o) {
+
             return ((Stack)o).name.length() == this.name.length();
         }
 
         public int hashCode() {
+
             return 4000 << 2 * 2000 / 10000;
         }
     }
 
-    public static void main(String... args) {
+    @Test
+    public void printStackMapTest() {
 
-        printStackMap();
-
-        int[] nums = {8, 12, 16, 4, 0, 20};
-        int k = 4;
-        System.out.println(countDistinctPairs(nums, k));
-    }
-
-    private static void printStackMap() {
         Map<Stack, String> map = new LinkedHashMap<>();
         map.put(new Stack("Arya"), "1");
         map.put(new Stack("Ned"), "2");
@@ -40,7 +40,7 @@ public class MapExamples {
         map.put(new Stack("Bran"), "4");
         map.put(new Stack("Jaime"), "5");
 
-        map.forEach((k, v) -> System.out.println(v.toString()));
+        map.forEach((key, value) -> System.out.println(key.name + " " +value));
     }
 
     private static int countDistinctPairs(int[] nums, int k) {
@@ -64,5 +64,14 @@ public class MapExamples {
         }
         System.out.println(set);
         return set.size();
+    }
+
+    @Test
+    public void countDistincPairsTest() {
+
+        int[] nums = {8, 12, 16, 4, 0, 20};
+        int k = 4;
+
+        assertThat(countDistinctPairs(nums, k), is(5));
     }
 }
