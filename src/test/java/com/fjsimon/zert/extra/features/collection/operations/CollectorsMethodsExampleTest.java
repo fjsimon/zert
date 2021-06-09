83db4d04ae8e049546c4fa225fd2869ec98451a1
fjsimon <fjlopez.mail@gmail.com>
2021-06-09 15:58:18 +0100

    Refactor: moving src to test (collectors Methods examples)

--- src/test/java/com/fjsimon/zert/extra/features/collection/operations/CollectorsMethodsExampleTest.java
+++ src/test/java/com/fjsimon/zert/extra/features/collection/operations/CollectorsMethodsExampleTest.java
@@ -4,33 +4,100 @@ import com.fjsimon.zert.extra.features.common.FuturamaCharacter;
 import com.fjsimon.zert.extra.features.common.Species;
 import org.junit.Test;
 
-import java.util.IntSummaryStatistics;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+
 
 public class CollectorsMethodsExampleTest {
 
+    private final List<FuturamaCharacter> characters = Arrays.asList(
+            new FuturamaCharacter("Bender", "Rodriguez", 5, Optional.of(Species.ROBOT)),
+            new FuturamaCharacter("Philip", "Fry", 126, Optional.of(Species.HUMAN)),
+            new FuturamaCharacter("Turanga", "Leela", 22, Optional.of(Species.MUTANT)),
+            new FuturamaCharacter("Hubert", "Farnsworth", 161, Optional.of(Species.HUMAN)));
+
     @Test
-    public void futuramaCharactersCollectorsTest(){
+    public void getYoungerThan100Years() {
 
-        CollectorsMethodsExample ex = new CollectorsMethodsExample();
-        List<FuturamaCharacter> characters = ex.getFuturamaCharacters();
-        assertThat(characters.size(), is(4));
-        assertThat(ex.getYoungerThan100Years(characters).size(), is(2));
-        assertThat(ex.getAverageAge(characters), is(78.5));
+        System.out.println("\nYounger than 100 years:");
+        List<FuturamaCharacter> youngerThan100Years = characters
+                .stream()
+                .filter(c -> c.getAge() < 100)
+                // sorted by age DESC
+                .sorted((c1, c2) -> c2.getAge().compareTo(c1.getAge()))
+                //.collect(Collectors.toList());
+                .collect(Collectors.toCollection(ArrayList::new));
+        youngerThan100Years.forEach(System.out::println);
 
+        assertThat(youngerThan100Years.size(), is(2));
+    }
 
-        IntSummaryStatistics summaryStatistic = ex.getSummaryStatistic(characters);
-        assertThat(summaryStatistic.getCount(), is(4L));
-        assertThat(summaryStatistic.getSum(), is(314L));
-        assertThat(summaryStatistic.getAverage(), is(78.5));
-        assertThat(summaryStatistic.getMin(), is(5));
-        assertThat(summaryStatistic.getMax(), is(161));
+    @Test
+    public void getAverageAge() {
 
-        assertThat(ex.getSpeciesToFuturamaCharactersMap(characters).get(Species.HUMAN).size(), is(2));
-        assertThat(ex.getIsHumanToFuturamaCharactersMap(characters).get(Boolean.FALSE).size(), is(2));
-        assertThat(ex.getFirstnames(characters), is("Bender, Hubert, Philip, Turanga"));
+        Double averageAge = characters
+                .stream()
+                .collect(Collectors.averagingInt(FuturamaCharacter::getAge));
+        System.out.println("\nAverage age: " + averageAge);
+
+        assertThat(averageAge, is(78.5));
+    }
+
+    @Test
+    public void getSummaryStatistic() {
+
+        IntSummaryStatistics intSummaryStatistics = characters
+                .stream()
+                .collect(Collectors.summarizingInt(FuturamaCharacter::getAge));
+        System.out.println("\nintSummaryStatistics: " + intSummaryStatistics);
+        System.out.println("Sum of ages: " + intSummaryStatistics.getSum());
+
+        assertThat(intSummaryStatistics.getCount(), is(4L));
+        assertThat(intSummaryStatistics.getSum(), is(314L));
+        assertThat(intSummaryStatistics.getAverage(), is(78.5));
+        assertThat(intSummaryStatistics.getMin(), is(5));
+        assertThat(intSummaryStatistics.getMax(), is(161));
+    }
+
+    @Test
+    public void getSpeciesToFuturamaCharactersMap() {
+
+        Map<Species, List<FuturamaCharacter>> speciesToFuturamaCharactersMap = characters
+                .stream()
+                .collect(Collectors.groupingBy(c -> c.getSpeciesEnum().get()));
+        System.out.println("\nHumans: ");
+        speciesToFuturamaCharactersMap.get(Species.HUMAN).forEach(System.out::println);
+
+        assertThat(speciesToFuturamaCharactersMap.get(Species.HUMAN).size(), is(2));
+        assertThat(speciesToFuturamaCharactersMap.get(Species.MUTANT).size(), is(1));
+        assertThat(speciesToFuturamaCharactersMap.get(Species.ROBOT).size(), is(1));
+    }
+
+    @Test
+    public void getIsHumanToFuturamaCharactersMap() {
+
+        Map<Boolean, List<FuturamaCharacter>> isHumanToFuturamaCharactersMap = characters
+                .stream()
+                .collect(Collectors.partitioningBy(c -> Species.HUMAN.equals(c.getSpeciesEnum().get())));
+        System.out.println("\nNon-humans: ");
+        isHumanToFuturamaCharactersMap.get(Boolean.FALSE).forEach(System.out::println);
+
+        assertThat(isHumanToFuturamaCharactersMap.get(Boolean.FALSE).size(), is(2));
+    }
+
+    @Test
+    public void getFirstnames() {
+
+        String firstNames = characters
+                .stream()
+                .map(FuturamaCharacter::getFirstName)
+                .sorted()
+                .collect(Collectors.joining(", "));
+        System.out.println("\nFirst names: " + firstNames);
+
+        assertThat(firstNames, is("Bender, Hubert, Philip, Turanga"));
     }
 }
