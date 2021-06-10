bb0263fe4905a035eb94113a9a9d6bd3106d0397
fjsimon <fjlopez.mail@gmail.com>
2021-06-10 13:29:22 +0100

    Refactor: moving src to test (collectors Extract examples)

--- src/test/java/com/fjsimon/zert/extra/features/collection/operations/ExtractDataWithMapExampleTest.java
+++ src/test/java/com/fjsimon/zert/extra/features/collection/operations/ExtractDataWithMapExampleTest.java
@@ -3,28 +3,59 @@ package com.fjsimon.zert.extra.features.collection.operations;
 import com.fjsimon.zert.extra.features.common.FuturamaCharacter;
 
 import org.junit.Test;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.hamcrest.core.IsNot.not;
-import static org.hamcrest.core.IsNull.nullValue;
-import static org.junit.Assert.assertThat;
 
 public class ExtractDataWithMapExampleTest {
 
+    private final List<FuturamaCharacter> characters = Arrays.asList(
+            new FuturamaCharacter("Bender", "Rodriguez", "robot"),
+            new FuturamaCharacter("Philip", "Fry", "human"),
+            new FuturamaCharacter("Turanga", "Leela", "mutant"),
+            new FuturamaCharacter("Zapp", "Brannigan", "human"));
+
     @Test
-    public void extractDataWithMapExampleTest(){
+    public void getFuturamaCharacters() {
 
-        ExtractDataWithMapExample ex = new ExtractDataWithMapExample();
-        List<FuturamaCharacter> characters = ex.getFuturamaCharacters();
+        System.out.println("Futurama characters:");
+        characters.forEach(System.out::println); // c -> System.out.println(c)
         assertThat(characters.size(), is(4));
+    }
 
-        assertThat(ex.getStream(characters).count(), is(3L));
-        System.out.println("\nDistinct Futurama character species:");
-        ex.getStream(characters).forEach(System.out::println);
+    @Test
+    public void getStream() {
 
-        assertThat(ex.getIntStream(characters).count(), is(4L));
-        System.out.println("\nHashcodes:");
-        ex.getIntStream(characters).forEach(System.out::println);
+        Stream<String> speciesStream = characters.stream()
+                .peek(c -> System.out.println("Filtered value: " + c))
+                // <R> Stream<R> map(Function<? super T, ? extends R> mapper);
+                .map(FuturamaCharacter::getSpecies) // c -> c.getSpecies()
+                .peek(s -> System.out.println("Mapped value: " + s.toUpperCase()))
+                .distinct()
+                .sorted();
+
+        assertThat(speciesStream.count(), is(3L));
+
+//        speciesStream.forEach(System.out::println);
+//        java.lang.IllegalStateException: stream has already been operated upon or closed
+    }
+
+    @Test
+    public void getIntStream() {
+
+        IntStream hashcodeStream = characters.stream()
+                // IntStream mapToInt(ToIntFunction<? super T> mapper);
+                .mapToInt(FuturamaCharacter::hashCode) // c -> c.hashCode()
+                .peek(hc -> System.out.println("Mapped value: " + hc))
+                .sorted();
+
+        assertThat(hashcodeStream.count(), is(4L));
+
+//        hashcodeStream.forEach(System.out::println);
+//        java.lang.IllegalStateException: stream has already been operated upon or closed
     }
 }
