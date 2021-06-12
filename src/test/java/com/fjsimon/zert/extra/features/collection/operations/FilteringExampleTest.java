ba30ca5b2a6a0384888cbdda77fbbb391820646c
fjsimon <fjlopez.mail@gmail.com>
2021-06-12 20:07:18 +0100

    Refactor: moving src to test (Filtering examples)

--- src/test/java/com/fjsimon/zert/extra/features/collection/operations/FilteringExampleTest.java
+++ src/test/java/com/fjsimon/zert/extra/features/collection/operations/FilteringExampleTest.java
@@ -3,24 +3,52 @@ package com.fjsimon.zert.extra.features.collection.operations;
 import com.fjsimon.zert.extra.features.common.FuturamaCharacter;
 import org.junit.Test;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Predicate;
 
+import static java.util.stream.Collectors.toList;
+import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+
 
 public class FilteringExampleTest {
 
+    private final List<FuturamaCharacter> characters = Arrays.asList(
+            new FuturamaCharacter("Bender", "Rodriguez", "robot"),
+            new FuturamaCharacter("Philip", "Fry", "human"),
+            new FuturamaCharacter("Turanga", "Leela", "mutant"),
+            new FuturamaCharacter("John", "Simon", "robot"));
+
     @Test
-    public void filteringTest(){
+    public void getFuturamaCharactersBySpecies() {
 
-        FilteringExample ex = new FilteringExample();
-        List<FuturamaCharacter> characters = ex.getFuturamaCharacters();
-        assertThat(characters.size(), is(4));
-        assertThat(ex.getFuturamaCharactersBySpecies(characters, "robot").size(), is(2));
-        assertThat(ex.getFuturamaCharactersBySpecies(characters, "human").size(), is(1));
-        assertThat(ex.getFuturamaCharactersBySpecies(characters, "mutant").size(), is(1));
+        assertThat(characters.stream()
+                .filter(c -> c.getSpecies().equals("robot"))
+                .collect(toList()).size(), is(2));
+        assertThat(characters.stream()
+                .filter(c -> c.getSpecies().equals("human"))
+                .collect(toList()).size(), is(1));
+        assertThat(characters.stream()
+                .filter(c -> c.getSpecies().equals("mutant"))
+                .collect(toList()).size(), is(1));
+    }
 
-        assertThat(ex.getMutantFuturamaCharacters(characters).size(), is(1));
+    @Test
+    public void getMutantFuturamaCharacters() {
 
+        assertThat(characters.stream()
+                .peek(c -> System.out.println("Filtered value: " + c))
+                .filter(new MutantSpeciesPredicate())
+                .collect(toList()).size(), is(1));
+
+    }
+
+    private class MutantSpeciesPredicate implements Predicate<FuturamaCharacter> {
+
+        @Override
+        public boolean test(FuturamaCharacter character) {
+            return "mutant".equals(character.getSpecies());
+        }
     }
 }
