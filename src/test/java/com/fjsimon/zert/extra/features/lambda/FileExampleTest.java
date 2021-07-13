79c7cfb8c2d16d37ffaab887c203789e33ecfd91
fjsimon <fjlopez.mail@gmail.com>
2021-07-13 19:03:21 +0100

    Refactor: moving src to test (file examples)

--- src/test/java/com/fjsimon/zert/extra/features/lambda/FileExampleTest.java
+++ src/test/java/com/fjsimon/zert/extra/features/lambda/FileExampleTest.java
@@ -1,8 +1,10 @@
 package com.fjsimon.zert.extra.features.lambda;
 
-import java.io.File;
-import java.io.IOException;
+import org.junit.Test;
 
+import java.io.File;
+
+import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,9 +15,13 @@ import java.util.function.BiPredicate;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class FilesApiImprovementsExample {
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
 
-    public static void main(String... args) throws IOException {
+public class FileExampleTest {
+
+    @Test
+    public void fileTest() throws IOException {
 
         Path start = new File(".").toPath();
         int maxDepth = Integer.MAX_VALUE;
@@ -27,13 +33,13 @@ public class FilesApiImprovementsExample {
         pathStream.sorted().forEach(System.out::println);
 
         List<Path> paths = Files.walk(start)
-                .filter(p -> p.toString().contains("FilesApiImprovementsExample.java"))
+                .filter(p -> p.toString().contains("FileExampleTest.java"))
                 .collect(Collectors.toList());
-        System.out.println("\nFile path: " + paths.get(0));
+
+        assertThat(paths.get(0).toString(), is("./src/test/java/com/fjsimon/zert/extra/features/lambda/FileExampleTest.java"));
 
         System.out.println("\nFile content: ");
         Files.lines(paths.get(0), Charset.defaultCharset())
                 .forEach(System.out::println);
-
     }
 }
