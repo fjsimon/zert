fcac3ab7f036930dcd0c80daaa46a3e8c4ed85be
fjsimon <fjlopez.mail@gmail.com>
2021-11-25 13:26:21 +0000

    fjsimon | updated ArrayListExamplesTest.java

--- src/test/java/com/fjsimon/zert/datatypes/IntegerTest.java
+++ src/test/java/com/fjsimon/zert/datatypes/IntegerTest.java
@@ -3,6 +3,11 @@ package com.fjsimon.zert.datatypes;
 import com.fjsimon.zert.BaseTest;
 import org.junit.Test;
 
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 public class IntegerTest extends BaseTest {
 
     public static void op(Integer obj, int var) {
@@ -15,7 +20,7 @@ public class IntegerTest extends BaseTest {
     }
 
     @Test
-    public void integerTest() {
+    public void integer_test() {
 
         Integer val1 = new Integer(5);
         int val2 = 9;
@@ -23,4 +28,39 @@ public class IntegerTest extends BaseTest {
         System.out.printf("val1 = %d, val2 = %d%n", val1, val2);
     }
 
+    @Test
+    public void integer_equals_test() {
+
+        Integer val1 = new Integer(5);
+        Integer val2 = Integer.parseInt("5");
+        System.out.printf("equals = %b %n", val1.equals(val2));
+        System.out.printf("hasCode val1 = %d %n", val1.hashCode());
+        System.out.printf("hasCode val2 = %d %n", val2.hashCode());
+    }
+
+
+    @Test
+    public void integer_compareTo_test() {
+
+        Integer val1 = new Integer(2);
+        Integer val2 = new Integer(5);
+        System.out.printf("compareTo = %d%n", val1.compareTo(val2));
+        System.out.printf("compareTo = %d%n", val2.compareTo(val1));
+        System.out.printf("compareTo = %d%n", val2.compareTo(val2));
+    }
+
+    @Test
+    public void integer_array_equals_test() {
+
+        Integer[] one = new Integer[10];
+        Integer[] two = new Integer[10];
+        assertThat(Arrays.equals(one, two), is(true));
+        one[0] = 1;
+        assertThat(Arrays.equals(one, two), is(false));
+        two[0] = 1;
+        assertThat(Arrays.equals(one, two), is(true));
+        one[1] = 2;
+        two[0] = 2;
+        assertThat(Arrays.equals(one, two), is(false));
+    }
 }
