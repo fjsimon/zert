40680988f2863e7edcb6ad6ca618e7a05e2ab1e2
fjsimon <fjlopez.mail@gmail.com>
2025-09-11 08:57:54 +0100

    Added chapter 1 questions

---
+++ src/test/java/com/ocp/zert/chapter1_quetions/Q1.java
@@ -0,0 +1,20 @@
+package com.ocp.zert.chapter1_quetions;
+
+public class Q1 {
+    public int employeeId;
+    public String firstName, lastName;
+    public int yearStarted;
+    @Override public int hashCode() {
+        return employeeId;
+    }
+    public boolean equals(Q1 e) {
+        return this.employeeId == e.employeeId;
+    }
+    public static void main(String[] args) {
+        Q1 one = new Q1();
+        one.employeeId = 101;
+        Q1 two = new Q1();
+        two.employeeId = 101;
+        if (one.equals(two)) System.out.println("Success");
+        else System.out.println("Failure");
+    } }
